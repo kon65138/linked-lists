5e74fb6ad55ab1114cf886a9c88b22b69b06969b
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-17 16:20:27 +0000

    add size method to linked list to return total nubmer of nodes in list

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ class LinkedList {
 
   append(value) {
     let head = this.head;
-    // recursivly goes from head through the nodes
-    // till it reaches a node without a next then returns that node
     function recSearch(node = head) {
       if (node.nextNode === null) return node;
       return recSearch(node.nextNode);
@@ -19,6 +17,15 @@ class LinkedList {
   prepend(value) {
     let temp = new Node(value, this.head);
     this.head = temp;
+  }
+
+  size() {
+    let head = this.head;
+    function recursiveCount(node = head, index = 1) {
+      if (node.nextNode === null) return index;
+      return recursiveCount(node.nextNode, (index += 1));
+    }
+    return recursiveCount();
   }
 }
 
@@ -36,3 +43,4 @@ theLiiist.append('the end lol');
 theLiiist.prepend('THE NEW HEAD HAHAHA');
 theLiiist.append('THIS STILL WORKS RIGHT');
 console.log(theLiiist);
+console.log(theLiiist.size());
