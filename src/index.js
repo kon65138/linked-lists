ee3bb9fc6685123ee03639408702692e79668fc4
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-17 16:00:38 +0000

    add prepend method to linked list

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
 import './style.css';
 
-class linkedList {
+class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
 
   append(value) {
     let head = this.head;
+    // recursivly goes from head through the nodes
+    // till it reaches a node without a next then returns that node
     function recSearch(node = head) {
       if (node.nextNode === null) return node;
       return recSearch(node.nextNode);
     }
-    recSearch().nextNode = new node(value);
+    recSearch().nextNode = new Node(value);
+  }
+
+  prepend(value) {
+    let temp = new Node(value, this.head);
+    this.head = temp;
   }
 }
 
-class node {
+class Node {
   constructor(value = null, nextNode = null) {
     this.value = value;
     this.nextNode = nextNode;
   }
 }
-let theHeaaad = new node('hi im head');
-let theLiiist = new linkedList(theHeaaad);
+let theHeaaad = new Node('hi im head');
+let theLiiist = new LinkedList(theHeaaad);
 theLiiist.append('the netxt noddde');
 theLiiist.append('the next neeext node');
 theLiiist.append('the end lol');
+theLiiist.prepend('THE NEW HEAD HAHAHA');
+theLiiist.append('THIS STILL WORKS RIGHT');
 console.log(theLiiist);
