f10c05db743d2ff1cda9f8e9a649862234288f00
bamboo_shoot <156806318+kon65138@users.noreply.github.com>
2025-02-17 15:53:29 +0000

    add append method to linked list class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import './style.css';
 
-console.log('hello world!');
-
 class linkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  append(value) {
+    let head = this.head;
+    function recSearch(node = head) {
+      if (node.nextNode === null) return node;
+      return recSearch(node.nextNode);
+    }
+    recSearch().nextNode = new node(value);
+  }
 }
 
 class node {
-  constructor(value) {
+  constructor(value = null, nextNode = null) {
     this.value = value;
-    this.nextNode = null;
+    this.nextNode = nextNode;
   }
 }
+let theHeaaad = new node('hi im head');
+let theLiiist = new linkedList(theHeaaad);
+theLiiist.append('the netxt noddde');
+theLiiist.append('the next neeext node');
+theLiiist.append('the end lol');
+console.log(theLiiist);
